61bd0a25abd96a5c0d0c83b059e033726b9f54c6
Andreas Naoum <andreas.naoum99@icloud.com>
2025-02-28 10:07:17 +0100

    vis demo on page test

--- docs/src/demo1.js
+++ docs/src/demo1.js
@@ -1,6 +1,6 @@
 import { WebViewer } from "@rerun-io/web-viewer";
 
-const rrdUrl = "https://raw.githubusercontent.com/andreasnaoum/reflex-viz/main/Recordings/test1.rrd";
+const rrdUrl = "https://raw.githubusercontent.com/andreasnaoum/reflex-viz/main/Recordings/sample.rrd";
 const parentElement = document.getElementById("demo1");
 
 const viewer = new WebViewer();
